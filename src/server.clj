c128f9dd6e5220b80b766c6c82b897dbad53a584
Andrew Downing <ad510n1@gmail.com>
2015-01-04 23:47:12 -0800

    show form on every page

--- src/server.clj
+++ src/server.clj
@@ -6,25 +6,24 @@
 
 (def content-type [:meta {:http-equiv "content-type" :content "text/html; charset=utf-8"}])
 
-(def home (html5 [:head [:title "Number Analyzer"] content-type]
-                 [:body [:p "Enter a number: " (text-field "number")
-                            [:button {:onclick "location = document.getElementById('number').value"} "Submit"]]]))
+(def number-form [:p "Enter a number: " (text-field "number")
+                     [:button {:onclick "location = document.getElementById('number').value"} "Submit"]])
 ;(form-to [:get ""] (text-field "number") (submit-button "Submit"))
 
-
 (defroutes handler
-  (GET "/" [] home)
+  (GET "/" [] (html5 [:head [:title "Number Analyzer"] content-type]
+                     [:body number-form]))
   (GET "/:uri" [uri]
     (let [id (try (bigdec uri) (catch NumberFormatException ex uri))]
-      (if (number? id)
-        (html5 [:head [:title (escape-html id)] content-type]
-               [:body [:h1 (escape-html id) " is "
-                        (if (zero? id) "zero"
-                            (str (if (pos? id) "a positive " "a negative ")
-                                 (if (zero? (rem id 1)) "integer" "decimal")))]
-                      (cond (and (pos? id) (zero? (rem id 1)))
-                            [:p "Factors of " (escape-html id) ": "
-                                (interpose ", " (filter #(zero? (rem id %)) (range 1 (inc (Math/sqrt id)))))])])
-        home))))
+      (html5 [:head [:title (escape-html id)] content-type]
+             [:body number-form
+                    [:h1 (escape-html id) " is "
+                         (cond (not (number? id)) "not a number"
+                               (zero? id) "zero"
+                               :else (str (if (pos? id) "a positive " "a negative ")
+                                          (if (zero? (rem id 1)) "integer" "decimal")))]
+                    (cond (and (number? id) (pos? id) (zero? (rem id 1)))
+                          [:p "Factors of " (escape-html id) ": "
+                              (interpose ", " (filter #(zero? (rem id %)) (range 1 (inc (Math/sqrt id)))))])]))))
 
 (defonce server (run-jetty #'handler {:port 8000 :join? false}))
