c9fc2364e938d569b18f1a68b910b5a0c39090cd
Andrew Downing <ad510n1@gmail.com>
2015-01-04 22:09:50 -0800

    classify numbers

--- src/server.clj
+++ src/server.clj
@@ -13,8 +13,11 @@
   (GET "/:uri" [uri]
     (let [id (try (bigdec uri) (catch NumberFormatException ex uri))]
       (if (number? id)
-        (html5 [:head [:title "number"] content-type]
-               [:body [:p "hello, " (escape-html id)]])
+        (html5 [:head [:title (escape-html id)] content-type]
+               [:body [:h1 (escape-html id) " is "
+                        (if (zero? id) "zero"
+                            (str (if (pos? id) "a positive " "a negative ")
+                                 (if (zero? (rem id 1)) "integer" "decimal")))]])
         home))))
 
 (defonce server (run-jetty #'handler {:port 8000 :join? false}))
