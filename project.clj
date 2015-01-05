de351ee29d30f7648237822e73d5d10736430c51
Andrew Downing <ad510n1@gmail.com>
2015-01-04 20:38:14 -0800

    output html with hiccup

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.2"]
                  ;[compojure "1.3.1"]
-                 ;[hiccup "1.0.5"]
-])
+                 [hiccup "1.0.5"]])
--- src/server.clj
+++ src/server.clj
@@ -1,6 +1,11 @@
-(use '[ring.adapter.jetty :only [run-jetty]])
+(use '[ring.adapter.jetty :only [run-jetty]]
+     'hiccup.page)
 
 (defn handler [req]
-  {:body "hello world" :headers {} :status 200})
+  {:body (html5 [:head [:title "title"]
+                       [:meta {:http-equiv "content-type" :content "text/html; charset=utf-8"}]]
+                [:body [:p "hello world"]])
+   :headers {}
+   :status 200})
 
 (defonce server (run-jetty #'handler {:port 8000 :join? false}))
