696db130db403798a0537ec1292d20169488f21c
Andrew Downing <ad510n1@gmail.com>
2015-01-04 23:56:10 -0800

    rename project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject webapp-test "0.1.0"
+(defproject number-analyzer "0.1.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.2"]
                  [compojure "1.3.1"]
